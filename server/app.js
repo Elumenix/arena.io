b288dda877a23ff0988c24a4785e19f156c2416f
Donovan Scullion (RIT Student) <89927445+Elumenix@users.noreply.github.com>
2023-11-15 13:52:15 -0500

    Finally got server to connect to client

--- server/app.js
+++ server/app.js
@@ -1,8 +1,8 @@
 //require('dotenv').config();
 
-/*const path = require('path');
+const path = require('path');
 const express = require('express');
-const compression = require('compression');
+/*const compression = require('compression');
 const favicon = require('serve-favicon');
 const bodyParser = require('body-parser');*/
 const mongoose = require('mongoose');
@@ -13,30 +13,43 @@ const RedisStore = require('connect-redis').default;
 const redis = require('redis');*/
 
 //const router = require('./router.js');
-
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-const dbURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1/DomoMaker';
-mongoose.connect(dbURI).catch((err) => {
-  if (err) {
+const dbURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1/Arena';
+
+
+//mongoose.connect(dbURI).catch((err) => {
+  /*if (err) {
     console.log('Could not connect to database');
     throw err;
-  }
-});
+  }*/
 
-const redisClient = redis.createClient({
+  const app = express();
+
+  // Everything under here should be deleted when redis is initialized
+  app.get('/', function(req, res) {
+    res.sendFile(path.resolve('client/index.html'));
+  });
+
+  app.listen(port, (err) => {
+    if (err) { throw err; }
+    console.log(`Listening on port ${port}`);
+  });
+//});
+
+/*const redisClient = redis.createClient({
   url: process.env.REDISCLOUD_URL,
 });
 
-redisClient.on('error', (err) => console.log('Redis Client Error', err));
+redisClient.on('error', (err) => console.log('Redis Client Error', err));*/
 
-redisClient.connect().then(() => {
+/*redisClient.connect().then(() => {
   const app = express();
 
   app.use(helmet());
   app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
-  app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
-  app.use(compression());
+  //app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
+  //app.use(compression());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
 
@@ -60,4 +73,4 @@ redisClient.connect().then(() => {
     if (err) { throw err; }
     console.log(`Listening on port ${port}`);
   });
-});
+});*/
